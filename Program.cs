77df1f66d3e8042581f1f14863923690a5e0e760
Diyrbek <gulzigitovdajyr@gmail.com>
2024-04-02 16:21:48 +0600

    task2

--- Program.cs
+++ Program.cs
@@ -1,44 +1,44 @@
-﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+﻿// //  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
-Console.Write($"Введите значение N: ");
-int N=Convert.ToInt32(Console.ReadLine());
-Console.Write($"Введите значение M: ");
-int M=Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Введите значение N: ");
+// int N=Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Введите значение M: ");
+// int M=Convert.ToInt32(Console.ReadLine());
 
-void PrintLimitedNumbers(int start, int end)
-{
-    if (start == end)
-    {
-       Console.Write(start); 
-       return;
-    } 
-
-    Console.Write(start + " ");
-
-    PrintLimitedNumbers(start + 1, end);
-
-}
-
-PrintLimitedNumbers(N, M);
-
-
-
-
-// // Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-
-// Console.Write("Введите значение m: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите значение n: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// int A(int m, int n)
+// void PrintLimitedNumbers(int start, int end)
 // {
-//     if (m == 0) return n + 1;
-//     else if (n == 0) return A(m - 1, 1);
-//     else return A(m - 1, A(m, n - 1));
+//     if (start == end)
+//     {
+//        Console.Write(start); 
+//        return;
+//     } 
+
+//     Console.Write(start + " ");
+
+//     PrintLimitedNumbers(start + 1, end);
+
 // }
 
-// Console.Write(A(m, n));
+// PrintLimitedNumbers(N, M);
+
+
+
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return A(m - 1, 1);
+    else return A(m - 1, A(m, n - 1));
+}
+
+Console.Write(A(m, n));
 
 
 
