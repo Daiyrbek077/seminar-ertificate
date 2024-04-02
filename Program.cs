91af964c4b26e46c542694b3e74732ed55a88175
Diyrbek <gulzigitovdajyr@gmail.com>
2024-04-02 16:22:20 +0600

    task3

--- Program.cs
+++ Program.cs
@@ -24,38 +24,38 @@
 
 
 
-// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-Console.Write("Введите значение m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int A(int m, int n)
-{
-    if (m == 0) return n + 1;
-    else if (n == 0) return A(m - 1, 1);
-    else return A(m - 1, A(m, n - 1));
-}
-
-Console.Write(A(m, n));
-
-
-
-// // Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
-
-// int[] array = { 1, 2, 5, 0, 10, 34 };
-// int size = array.Length-1;
-// void ShowArray(int[] arr, int size)
+// int A(int m, int n)
 // {
-//     if (size == 0)
-//     {
-//         Console.Write(arr[size]);
-//         return;
-//     }
-//     Console.Write(arr[size]+" ");
-
-//     ShowArray(arr, size-1);
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return A(m - 1, 1);
+//     else return A(m - 1, A(m, n - 1));
 // }
-// ShowArray(array, size);
+
+// Console.Write(A(m, n));
+
+
+
+// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+int size = array.Length-1;
+void ShowArray(int[] arr, int size)
+{
+    if (size == 0)
+    {
+        Console.Write(arr[size]);
+        return;
+    }
+    Console.Write(arr[size]+" ");
+
+    ShowArray(arr, size-1);
+}
+ShowArray(array, size);
 
